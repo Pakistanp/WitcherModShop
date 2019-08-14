096bfc427a0e4c9690f8b8f4db8022ae10bf8370
Pakistanp <ppp16@vp.pl>
2019-08-14 16:17:50 +0200

    init

---
+++ src/main/java/com/WitcherModShop/WitcherModShop/WitcherModShopApplication.java
@@ -0,0 +1,13 @@
+package com.WitcherModShop.WitcherModShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WitcherModShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WitcherModShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/WitcherModShop/WitcherModShop/WitcherModShopApplicationTests.java
@@ -0,0 +1,16 @@
+package com.WitcherModShop.WitcherModShop;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WitcherModShopApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
